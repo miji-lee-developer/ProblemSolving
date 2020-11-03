83fa958fd4d9fc7dc5d248967648f6b9285bcabb
miji <miji.lee.developer@gmail.com>
2020-11-03 19:46:46 +0900

    Day 2. Taum and B'day

---
+++ coding_test/taum_and_bday.py
@@ -0,0 +1,53 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'taumBday' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER b
+#  2. INTEGER w
+#  3. INTEGER bc
+#  4. INTEGER wc
+#  5. INTEGER z
+#
+
+def taumBday(b, w, bc, wc, z):
+    res = 0
+
+    if bc > wc + z:
+        res = (b * (wc + z)) + (w * wc)
+    elif wc > bc + z:
+        res = (b * bc) + (w * (bc + z))
+    else:
+        res = (b * bc) + (w * wc)
+
+    return res
+
+if __name__ == '__main__':
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        b = int(first_multiple_input[0])
+
+        w = int(first_multiple_input[1])
+
+        second_multiple_input = input().rstrip().split()
+
+        bc = int(second_multiple_input[0])
+
+        wc = int(second_multiple_input[1])
+
+        z = int(second_multiple_input[2])
+
+        result = taumBday(b, w, bc, wc, z)
+
+        print(str(result) + '\n')
