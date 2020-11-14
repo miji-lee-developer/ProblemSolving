8e4f89612def63800780e200cc642b780e37324e
miji <miji.lee.developer@gmail.com>
2020-11-14 23:09:29 +0900

    Day 13. Strong Password

---
+++ coding_test/strong_password.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumNumber function below.
+def minimumNumber(n, password):
+    res = 0
+
+    return res
+
+if __name__ == '__main__':
+    n = int(input())
+
+    password = input()
+
+    answer = minimumNumber(n, password)
+
+    print(str(answer) + '\n')
