20ea35e6ff01ba1bd673d5051ab3578627f2bcc1
miji <miji.lee.developer@gmail.com>
2020-11-17 19:51:48 +0900

    Day 15. Strong Password

--- coding_test/strong_password.py
+++ coding_test/strong_password.py
@@ -8,39 +8,31 @@ import sys
 
 # Complete the minimumNumber function below.
 def minimumNumber(n, password):
-    ln = 6
-    if n <= 3:
-        return ln - n
-    else:
-        cl = [False, False, False, False]
-        special_char = "!@#$%^&*()-+"
-        for i in range(n):
-            if False in cl:
-                if 48 <= ord(password[i]) <= 57:
-                    cl[0] = True
-                elif 65 <= ord(password[i]) <= 90:
-                    cl[1] = True
-                elif password[i] in special_char:
-                    cl[2] = True
-                elif 97 <= ord(password[i]) <= 122:
-                    cl[3] = True
-            else:
-                break
-
+    ml = 6
+    cl = [False, False, False, False]
+    special_char = "!@#$%^&*()-+"
+    for i in range(n):
         if False in cl:
-            r = 0
-            for b in cl:
-                if b == False:
-                    r += 1
+            if 48 <= ord(password[i]) <= 57:
+                cl[0] = True
+            elif 65 <= ord(password[i]) <= 90:
+                cl[1] = True
+            elif password[i] in special_char:
+                cl[2] = True
+            elif 97 <= ord(password[i]) <= 122:
+                cl[3] = True
+        else:
+            break
 
-            if n < ln:
-                if (ln - n) > r:
-                    return ln - n
-                else:
-                    return r
-            else:
-                return r
+    cb = 0
+    for b in cl:
+        if b == False:
+            cb += 1
 
+    if ml - n > cb:
+        return ml - n
+    else:
+        return cb
 
 if __name__ == '__main__':
     n = int(input())
