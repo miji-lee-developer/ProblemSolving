1078fffdb657e1496b2af8740d1eb243f97931f4
miji <miji.lee.developer@gmail.com>
2020-12-01 19:13:50 +0900

    Day 29. Gemstones

--- coding_test/genstones.py
+++ coding_test/genstones.py
@@ -8,7 +8,19 @@ import sys
 
 # Complete the gemstones function below.
 def gemstones(arr):
-    return 0
+    res = 0
+    s_arr = ''.join(map(lambda x: ''.join(set(x)), arr))
+    d = {}
+    for i in range(len(s_arr)):
+        s = s_arr[i]
+        if s in d:
+            d[s] += 1
+            if d[s] == len(arr):
+                res += 1
+        else:
+            d[s] = 1
+
+    return res
 
 if __name__ == '__main__':
     n = int(input())
