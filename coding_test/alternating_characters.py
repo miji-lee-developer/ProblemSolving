ea1aff74d6a64a27517e6c3d59f58268b75cd53d
miji <miji.lee.developer@gmail.com>
2020-12-02 19:01:35 +0900

    Day 30. Alternating Characters

---
+++ coding_test/alternating_characters.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the alternatingCharacters function below.
+def alternatingCharacters(s):
+    return 0
+
+if __name__ == '__main__':
+    q = int(input())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = alternatingCharacters(s)
+
+        print(str(result) + '\n')
