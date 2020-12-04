c273de182973cb99466810523b55c49a67419a62
miji <miji.lee.developer@gmail.com>
2020-12-04 19:55:38 +0900

    Day 32. Beautiful Binary String

---
+++ coding_test/beautiful_binary_string.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the beautifulBinaryString function below.
+def beautifulBinaryString(b):
+    return 0
+
+if __name__ == '__main__':
+    n = int(input())
+
+    b = input()
+
+    result = beautifulBinaryString(b)
+
+    print(str(result) + '\n')
