fdb313cf10ea3f51674f415dcf192341977763f7
miji <miji.lee.developer@gmail.com>
2020-12-05 21:41:50 +0900

    Day 33. Beautiful Binary String

--- coding_test/beautiful_binary_string.py
+++ coding_test/beautiful_binary_string.py
@@ -8,7 +8,8 @@ import sys
 
 # Complete the beautifulBinaryString function below.
 def beautifulBinaryString(b):
-    return 0
+    res = 0
+    return res
 
 if __name__ == '__main__':
     n = int(input())
