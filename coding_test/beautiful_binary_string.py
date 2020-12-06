29dafc3b89340b136fd5c28caba254c26821789e
miji <miji.lee.developer@gmail.com>
2020-12-06 18:37:45 +0900

    Day 34. Beautiful Binary String

--- coding_test/beautiful_binary_string.py
+++ coding_test/beautiful_binary_string.py
@@ -9,6 +9,12 @@ import sys
 # Complete the beautifulBinaryString function below.
 def beautifulBinaryString(b):
     res = 0
+    s = '010'
+
+    while b.find(s) > -1:
+        b = b.replace(s, '111', 1)
+        res += 1
+
     return res
 
 if __name__ == '__main__':
