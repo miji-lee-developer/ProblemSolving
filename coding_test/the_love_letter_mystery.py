cf1559836b0c3c449d649129c4d08573090bbb6a
miji <miji.lee.developer@gmail.com>
2020-12-07 20:32:02 +0900

    Day 35. The Love-Letter Mystery

---
+++ coding_test/the_love_letter_mystery.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the theLoveLetterMystery function below.
+def theLoveLetterMystery(s):
+    return 0
+
+if __name__ == '__main__':
+    q = int(input())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = theLoveLetterMystery(s)
+
+        print(str(result) + '\n')
