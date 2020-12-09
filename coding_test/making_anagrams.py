f72c4231cefdf199be3b81a136bb1646e2d2e776
miji <miji.lee.developer@gmail.com>
2020-12-09 20:31:37 +0900

    Day 37. Making Anagrams

---
+++ coding_test/making_anagrams.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the makingAnagrams function below.
+def makingAnagrams(s1, s2):
+    return 0
+
+if __name__ == '__main__':
+    s1 = input()
+
+    s2 = input()
+
+    result = makingAnagrams(s1, s2)
+
+    print(str(result) + '\n')
