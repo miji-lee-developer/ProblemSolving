b4fb2ef8242856621a61ed4ab785f218d76e8729
miji <miji.lee.developer@gmail.com>
2020-12-12 22:10:01 +0900

    Day 40. Making Anagrams

--- coding_test/making_anagrams.py
+++ coding_test/making_anagrams.py
@@ -12,9 +12,11 @@ def makingAnagrams(s1, s2):
 
     for i in range(len(s1)):
         if s1[i] in s2:
+            # print(s1[i])
+            s2 = s2.replace(s1[i], '', 1)
             c += 1
 
-    return (len(s1) - c) + (len(s2) - c)
+    return (len(s1) - c) + len(s2)
 
 if __name__ == '__main__':
     s1 = input()
