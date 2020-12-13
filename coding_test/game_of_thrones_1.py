4541ff7e9baa9b6b4f580f6e764779d468ebe174
miji <miji.lee.developer@gmail.com>
2020-12-13 21:02:55 +0900

    Day 41. Game of Thrones 1

---
+++ coding_test/game_of_thrones_1.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the gameOfThrones function below.
+def gameOfThrones(s):
+    return ''
+
+if __name__ == '__main__':
+    s = input()
+
+    result = gameOfThrones(s)
+
+    print(result + '\n')
