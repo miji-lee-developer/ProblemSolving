db5f06b55266d9c781bb10c19f9db134fea71f7f
miji <miji.lee.developer@gmail.com>
2020-12-14 16:51:22 +0900

    Day 42. Game of Thrones 1

--- coding_test/game_of_thrones_1.py
+++ coding_test/game_of_thrones_1.py
@@ -8,7 +8,25 @@ import sys
 
 # Complete the gameOfThrones function below.
 def gameOfThrones(s):
-    return ''
+    d = {}
+    o_c = 0
+
+    for t in s:
+        if t in d:
+            d[t] += 1
+        else:
+            d[t] = 1
+
+    for v in d.values():
+        if v % 2 != 0:
+            o_c += 1
+
+    if len(s) % 2 == 0 and o_c > 0:
+        return 'NO'
+    elif len(s) % 2 != 0 and o_c > 1:
+        return 'NO'
+    else:
+        return 'YES'
 
 if __name__ == '__main__':
     s = input()
