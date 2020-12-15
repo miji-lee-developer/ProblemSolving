faa6f8e240e94273a8e59b9f42028aa312450e1a
miji <miji.lee.developer@gmail.com>
2020-12-15 19:56:19 +0900

    Day 43. String Construction

---
+++ coding_test/string_construction.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the stringConstruction function below.
+def stringConstruction(s):
+    return 0
+
+if __name__ == '__main__':
+    q = int(input())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = stringConstruction(s)
+
+        print(str(result) + '\n')
