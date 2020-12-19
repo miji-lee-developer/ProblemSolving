2f3071c52ef37ca3ff4f99049443a8eea73e5777
miji <miji.lee.developer@gmail.com>
2020-12-19 19:32:37 +0900

    Day 47. Ice Cream Parlor

--- coding_test/ice_cream_parlor.py
+++ coding_test/ice_cream_parlor.py
@@ -8,8 +8,10 @@ import sys
 
 # Complete the icecreamParlor function below.
 def icecreamParlor(m, arr):
-    res = []
-    return res
+    for i in range(len(arr) - 1):
+        for j in range(i + 1, len(arr)):
+            if arr[i] + arr[j] == m:
+                return i + 1, j + 1
 
 if __name__ == '__main__':
     t = int(input())
