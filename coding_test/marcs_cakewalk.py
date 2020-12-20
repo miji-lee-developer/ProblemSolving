d46a3fc9266e8ba8322e715c8f433949bbfaf295
miji <miji.lee.developer@gmail.com>
2020-12-20 19:58:43 +0900

    Day 48. Marc's Cakewalk

---
+++ coding_test/marcs_cakewalk.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the marcsCakewalk function below.
+def marcsCakewalk(calorie):
+    return 0
+
+if __name__ == '__main__':
+    n = int(input())
+
+    calorie = list(map(int, input().rstrip().split()))
+
+    result = marcsCakewalk(calorie)
+
+    print(str(result) + '\n')
