98a585dec8055b7a7fcdd0dd6146192cc68bafff
miji <miji.lee.developer@gmail.com>
2020-12-25 21:51:34 +0900

    Day 53. Luck Balance

--- coding_test/luck_balance.py
+++ coding_test/luck_balance.py
@@ -8,8 +8,25 @@ import sys
 
 # Complete the luckBalance function below.
 def luckBalance(k, contests):
-    res = 0
-    return res
+    v = sum(1 for i in range(len(contests)) if contests[i][1] == 1)
+
+    if k >= v:
+        return sum(contests[i][0] for i in range(len(contests)))
+    else:
+        c = 0
+        min_v = 0
+        sv = sum(1 for i in range(len(contests)) if contests[i][1] == 1) - k
+        contests = sorted(contests)
+
+        for i in range(len(contests)):
+            if c == sv:
+                break
+
+            if contests[i][1] == 1:
+                min_v += contests[i][0]
+                c += 1
+
+        return sum(contests[i][0] for i in range(len(contests))) - (min_v * 2)
 
 if __name__ == '__main__':
     nk = input().split()
