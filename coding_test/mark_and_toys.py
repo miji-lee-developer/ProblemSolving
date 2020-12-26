cfc0a3fef9eff775c3218e5f68175719d2b61f01
miji <miji.lee.developer@gmail.com>
2020-12-26 20:24:50 +0900

    Day 54. Mark and Toys

---
+++ coding_test/mark_and_toys.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the maximumToys function below.
+def maximumToys(prices, k):
+    return 0
+
+if __name__ == '__main__':
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    prices = list(map(int, input().rstrip().split()))
+
+    result = maximumToys(prices, k)
+
+    print(str(result) + '\n')
