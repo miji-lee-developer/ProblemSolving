43a393749aa427929002467b803f031221e13691
miji <miji.lee.developer@gmail.com>
2020-12-28 21:13:06 +0900

    Day 56. Mark and Toys

--- coding_test/mark_and_toys.py
+++ coding_test/mark_and_toys.py
@@ -9,6 +9,21 @@ import sys
 # Complete the maximumToys function below.
 def maximumToys(prices, k):
     res = 0
+    m = 0
+    prices = sorted(prices)
+
+    for i in range(len(prices)):
+        if i == 0:
+            if prices[i] <= k:
+                m = prices[i]
+                res += 1
+            else:
+                break
+        else:
+            if m + prices[i] <= k:
+                m += prices[i]
+                res += 1
+
     return res
 
 if __name__ == '__main__':
