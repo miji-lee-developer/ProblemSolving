6219de5e3b8237e08456579877330716cbbae984
ai-mijilee <60606545+ai-mijilee@users.noreply.github.com>
2021-01-04 18:51:30 +0900

    Day 63. Maximum Perimeter Triangle

--- coding_test/maximum_perimeter_triangle.py
+++ coding_test/maximum_perimeter_triangle.py
@@ -9,6 +9,17 @@ import sys
 # Complete the maximumPerimeterTriangle function below.
 def maximumPerimeterTriangle(sticks):
     res = []
+    sticks = sorted(sticks, reverse=True)
+    for i in range(len(sticks) - 2):
+        a = sticks[i]
+        b = sticks[i + 1]
+        c = sticks[i + 2]
+        if a + b > c and a + c > b and b + c > a:
+            res = [c, b, a]
+            break
+
+    if len(res) == 0:
+        res = [-1]
     return res
 
 if __name__ == '__main__':
